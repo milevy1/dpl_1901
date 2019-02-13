09babb15f3445e35bf5b6aa42a2139ccd52883a1
Matt Levy <milevy1@gmail.com>
2019-02-13 09:23:57 -0700

    Added test author can add books

--- test/author_test.rb
+++ test/author_test.rb
@@ -1,5 +1,6 @@
 require './test/test_helper'
 require './lib/author'
+require './lib/book'
 
 class AuthorTest < Minitest::Test
 
@@ -9,6 +10,18 @@ class AuthorTest < Minitest::Test
 
   def test_author_starts_with_no_books
     assert_equal [], @nk_jemisin.books
+  end
+
+  def test_author_can_add_books
+    @nk_jemisin.add_book("The Fifth Season", "November 3, 2015")
+    book_attributes = { author_first_name: "N.K",
+                        author_last_name: "Jesimin",
+                        title: "The Fifth Season",
+                        publication_date: "November 3, 2015" }
+
+    book = Book.new(book_attributes)
+
+    assert_equal [book], @nk_jemisin.books
   end
 
 end
