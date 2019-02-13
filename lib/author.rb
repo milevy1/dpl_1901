6ddbc76048869cd341ffc8e79629e3562b26f89a
Matt Levy <milevy1@gmail.com>
2019-02-13 09:17:07 -0700

    Added author attributes

--- lib/author.rb
+++ lib/author.rb
@@ -1,3 +1,10 @@
 class Author
+  attr_reader :books
+
+  def initialize(author_attributes)
+    @first_name = author_attributes[:first_name]
+    @last_name = author_attributes[:last_name]
+    @books = []
+  end
 
 end
