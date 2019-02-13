4c929734c0ddddcf1480f9c4f2ebcb9335f8d02e
Matt Levy <milevy1@gmail.com>
2019-02-13 09:13:47 -0700

    Added author setup

--- test/author_test.rb
+++ test/author_test.rb
@@ -1,6 +1,10 @@
 require './test/test_helper'
+require './lib/author'
 
 class AuthorTest < Minitest::Test
 
+  def setup
+    @nk_jemisin = Author.new({first_name: "N.K.", last_name: "Jemisin"})
+  end
 
 end
