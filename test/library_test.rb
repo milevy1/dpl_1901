a94111f2ccaa8479547f60f5da67fa710db473f4
Matt Levy <milevy1@gmail.com>
2019-02-13 09:39:49 -0700

    Updated setup with instance variables missing

--- test/library_test.rb
+++ test/library_test.rb
@@ -6,13 +6,17 @@ class LibraryTest < Minitest::Test
 
   def setup
     @nk_jemisin = Author.new({first_name: "N.K.", last_name: "Jemisin"})
-    @fifth_season = nk_jemisin.add_book("The Fifth Season", "November 3, 2015")
-    @kingdoms = nk_jemisin.add_book("The Hundred Thousand Kingdoms", "2010")
+    @fifth_season = @nk_jemisin.add_book("The Fifth Season", "November 3, 2015")
+    @kingdoms = @nk_jemisin.add_book("The Hundred Thousand Kingdoms", "2010")
 
     @harper_lee  = Author.new({first_name: "Harper", last_name: "Lee"})
-    @mockingbird = harper_lee.add_book("To Kill a Mockingbird", "July 11, 1960")
+    @mockingbird = @harper_lee.add_book("To Kill a Mockingbird", "July 11, 1960")
 
     @dpl = Library.new
+  end
+
+  def test_library_starts_with_no_books
+    assert_equal [], @dpl.books
   end
 
 end
