a6455050a8cfef62618518f98a44b8b73c236e5d
Matt Levy <milevy1@gmail.com>
2019-02-13 10:22:41 -0700

    Added find_by_author

--- lib/library.rb
+++ lib/library.rb
@@ -17,4 +17,15 @@ class Library
     @books.sort_by { |book| book.author_last_name }
   end
 
+  def find_by_author(author_full_name)
+    books_by_author = @books.select { |book| author_full_name == book.author_full_name }
+
+    grouped = books_by_author.group_by { |book| book.title }
+
+    flattened_group = {}
+    grouped.each { |key, value| flattened_group[key] = value[0] }
+
+    flattened_group
+  end
+
 end
